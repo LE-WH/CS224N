56b01c967ac4bc043a98472ea2342eef62e1a60b
leapeto <313305185@qq.com>
2022-06-23 22:40:19 +0800

    a4

---
+++ a4/myQuestions.md
@@ -0,0 +1,5 @@
+## So I still have some questions here...
+
+1. In decoder why we concatenate y_t with o_prev ? I don't get it, and still don't know how we use this model.
+
+Anyway I got the main idea.
---
+++ a4/test.py
@@ -0,0 +1,19 @@
+from collections import namedtuple
+import sys
+from typing import List, Tuple, Dict, Set, Union
+import torch
+import torch.nn as nn
+import torch.nn.utils
+import torch.nn.functional as F
+from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
+
+from model_embeddings import ModelEmbeddings
+
+
+
+a = (4,'1')
+
+b,c = a
+print(b)
+
+print(c)
